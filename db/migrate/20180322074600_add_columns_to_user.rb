b62e264844b4771e16931a5ca2ab042cd5bc83ff
carshow <saladvar1212@gmail.com>
2018-03-22 16:48:13 +0900

    Create User model
    Using Devise gem
    Add :name, :magazine columns

---
+++ db/migrate/20180322074600_add_columns_to_user.rb
@@ -0,0 +1,6 @@
+class AddColumnsToUser < ActiveRecord::Migration[5.0]
+  def change
+    add_column :Users, :name, :string, null: false, unique: true
+    add_column :Users, :magazine, :integer, null: false
+  end
+end
