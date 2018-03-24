44ed34b9bde81124c56adb1b4d0bbcff112732ef
carshow <saladvar1212@gmail.com>
2018-03-25 02:28:10 +0900

    Create Carousel-slider using "Slick" Plugin

---
+++ app/assets/javascripts/slide.js
@@ -0,0 +1,12 @@
+$(function() {
+    $('.slider').slick({
+          infinite: true,
+          dots: true,
+          slidesToShow: 1,
+          centerMode: true, //要素を中央寄せ
+          centerPadding:'12%', //両サイドの見えている部分のサイズ
+          autoplay:true, //自動再生
+          arrows: true,
+          draggable: true,
+     });
+});
