48e60a5d905c94738f13c66f68c830139049b362
carshow <saladvar1212@gmail.com>
2018-03-25 04:12:32 +0900

    Modify Dots view,scss

--- app/assets/javascripts/slide.js
+++ app/assets/javascripts/slide.js
@@ -3,11 +3,12 @@ $(function() {
      infinite: true,
      dots: true,
      slidesToShow: 1,
-     centerMode: true, //要素を中央寄せ
-     centerPadding:'12%', //両サイドの見えている部分のサイズ
-     autoplay:true, //自動再生
+     centerMode: true,
+     centerPadding:'12%',
+     autoplay:true,
      arrows: true,
      draggable: true,
+      dotsClass: 'slide-dots',
      nextArrow: '<img src="https://d1te0bovbq6epg.cloudfront.net/common/img/slider/img_yajirushi_right.svg" class="slide-arrow prev-arrow">',
      prevArrow: '<img src="https://d1te0bovbq6epg.cloudfront.net/common/img/slider/img_yajirushi_left.svg" class="slide-arrow next-arrow">'
   });
