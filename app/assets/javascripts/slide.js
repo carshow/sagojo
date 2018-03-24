75e0ec0b430d81f72675c7db082b1a23f3c17190
carshow <saladvar1212@gmail.com>
2018-03-25 03:36:31 +0900

    Add Arrow to Carousel-slider

--- app/assets/javascripts/slide.js
+++ app/assets/javascripts/slide.js
@@ -1,12 +1,14 @@
 $(function() {
-    $('.slider').slick({
-          infinite: true,
-          dots: true,
-          slidesToShow: 1,
-          centerMode: true, //要素を中央寄せ
-          centerPadding:'12%', //両サイドの見えている部分のサイズ
-          autoplay:true, //自動再生
-          arrows: true,
-          draggable: true,
-     });
+     $('.slider').slick({
+     infinite: true,
+     dots: true,
+     slidesToShow: 1,
+     centerMode: true, //要素を中央寄せ
+     centerPadding:'12%', //両サイドの見えている部分のサイズ
+     autoplay:true, //自動再生
+     arrows: true,
+     draggable: true,
+     nextArrow: '<img src="https://d1te0bovbq6epg.cloudfront.net/common/img/slider/img_yajirushi_right.svg" class="slide-arrow prev-arrow">',
+     prevArrow: '<img src="https://d1te0bovbq6epg.cloudfront.net/common/img/slider/img_yajirushi_left.svg" class="slide-arrow next-arrow">'
+  });
 });
