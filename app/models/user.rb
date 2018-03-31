bfd884d8de0274b41ff192c217d2c251421c3bfd
carshow <saladvar1212@gmail.com>
2018-03-31 14:39:03 +0900

    Add validates to sign_up check_box

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :confirmable, :omniauthable
 
   has_one :profile, dependent: :destroy
   has_many :applications
   has_many :job_tags, through: :user_jobtags
   has_many :articles, through: :clips
+
+  validates :check_box, acceptance: true
 end
