10bee25a5015d36c814a4917cc507ab897a46696
carshow <saladvar1212@gmail.com>
2018-04-01 14:22:29 +0900

    Validation

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,4 +3,18 @@ class Article < ApplicationRecord
   has_many :categories, through: :articles_categories
   has_many :users, through: :users
   has_many :application
+
+  validates :text, presence: true
+  validates :image, presence: true
+  validates :comment, presence: true
+  validates :price, presence: true
+  validates :title, presence: true
+  validates :overview, presence: true
+  validates :qualification, presence: true
+  validates :requested_number_of_people, presence: true
+  validates :period, presence: true
+  validates :destination, presence: true
+  validates :company_name, presence: true
+  validates :end_of_date, presence: true
+  validates :extra_comment, presence: true
 end
