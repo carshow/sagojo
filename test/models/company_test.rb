0c958e0f6144bac4f6f02c992cdf6c1466ec6cbd
carshow <saladvar1212@gmail.com>
2018-04-01 14:17:22 +0900

    Create company table

--- test/models/company_test.rb
+++ test/models/company_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class CompaniesControllerTest < ActionDispatch::IntegrationTest
+class CompanyTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
