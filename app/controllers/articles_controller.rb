cdaa39c90c8f9264fa2fa798777ba61129e43a6e
carshow <saladvar1212@gmail.com>
2018-04-01 14:22:20 +0900

    Modify articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  before_action :set_company, only: [:create, :new]
   def index
   end
 
@@ -15,5 +16,40 @@ class ArticlesController < ApplicationController
   end
 
   def business
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to root_path
+
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(
+     :text,
+     :image,
+     :comment,
+     :price,
+     :title,
+     :overview,
+     :qualification,
+     :requested_number_of_people,
+     :period,
+     :destination,
+     :company_name,
+     :end_of_date,
+     :extra_comment
+    ).merge(company_id: current_company.id)
+  end
+
+  def set_company
+    @company = Company.find_by(id: current_company.id)
   end
 end
