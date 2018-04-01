6b9fb31690e72e03f0035408d781290ee73a277b
carshow <saladvar1212@gmail.com>
2018-04-01 21:51:16 +0900

    Create clips controller

--- app/controllers/clips_controller.rb
+++ app/controllers/clips_controller.rb
@@ -1,2 +1,24 @@
 class ClipsController < ApplicationController
+  before_action :set_variables, only: [:create, :destroy]
+
+  def create
+    @clip = current_user.clips.new(article_id: @article.id)
+
+    if @clip.save
+      redirect_to root_path
+    end
+  end
+
+  def destroy
+    @clip = current_user.clips.find_by(article_id: @article.id)
+     if @clip.destroy
+      redirect_to root_path
+    end
+
+  end
+
+
+  def set_variables
+    @article = Article.find(params[:article_id])
+  end
 end
