815fc73c460b645025ed47b91450775f3d37641c
carshow <saladvar1212@gmail.com>
2018-04-01 20:58:06 +0900

    Change timezone

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,8 @@ Bundler.require(*Rails.groups)
 module Sagojo
   class Application < Rails::Application
     config.i18n.default_locale = :ja
+    config.active_record.default_timezone = :local
+    config.time_zone = 'Tokyo'
     config.generators do |g|
       g.assets     false
       g.helper     false
