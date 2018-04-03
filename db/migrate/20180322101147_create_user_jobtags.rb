31ecd2dbd6c981b4bc3c6bd3fd6ed02fd04a9a70
carshow <saladvar1212@gmail.com>
2018-04-03 17:16:59 +0900

    MIgration for tags & remove wasted table

--- db/migrate/20180322101147_create_user_jobtags.rb
+++
@@ -1,9 +0,0 @@
-class CreateUserJobtags < ActiveRecord::Migration[5.0]
-  def change
-    create_table :user_jobtags do |t|
-      t.references :user, foreign_key: true
-      t.references :job_tag, foreign_key: true
-      t.timestamps
-    end
-  end
-end
